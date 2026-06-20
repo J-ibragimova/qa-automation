722551778bd725efe15b963bd847891ccab2784e
ibrag <ibragiimova.j.e@gmail.com>
2026-06-20 23:25:08 +0300

    Add BaseTest with setUp/tearDown; update LoginTest

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,32 @@
+import org.openqa.selenium.*;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void checkLogin() throws InterruptedException {
+
+        driver.findElement(By.xpath("//*[@placeholder ='Username']")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//*[@placeholder ='Password']")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("#login-button")).click();
+        String titleName = driver.findElement(By.xpath("//*[@class='title']")).getText();
+        assertEquals(titleName, "Products", "Заголовок страницы не соответствует");
+
+    }
+    @Test
+    public void checkIncorrectLogin() {
+
+        driver.findElement(By.xpath("//*[@placeholder ='Username']")).sendKeys("");
+        driver.findElement(By.xpath("//input[@placeholder ='Password']")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("#login-button")).click();
+        boolean isTitleVisible = driver.findElement(By.xpath("//h3[@data-test='error']")).isDisplayed();
+        String errorText = driver.findElement(By.xpath("//h3[@data-test='error']")).getText();
+        assertTrue(isTitleVisible);
+        assertEquals(errorText, "Epic sadface: Username is required");
+    }
+
+}
+
+
